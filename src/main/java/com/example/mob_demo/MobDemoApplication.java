1c85c9639312db965ff4b239ea73195afcb79047
Kevin Gida <139972073+KevinGida@users.noreply.github.com>
2023-10-20 08:25:53 +0200

    initialize spring app

---
+++ src/main/java/com/example/mob_demo/MobDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mob_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobDemoApplication.class, args);
+    }
+
+}
